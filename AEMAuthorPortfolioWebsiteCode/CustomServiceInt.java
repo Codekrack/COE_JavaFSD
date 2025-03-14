dd98c930d111af50825aa6975c9fca122f46aeab
Codekrack <154899246+Codekrack@users.noreply.github.com>
2025-03-14 21:14:17 +0530

    Delete AEMAuthorPortfolioWebsiteCode directory

--- AEMAuthorPortfolioWebsiteCode/CustomServiceInt.java
+++
@@ -1,9 +0,0 @@
-package com.myTraining.core.services;
-
-public interface CustomServiceInt {
-    String getAuthorId();
-    String getAuthorApi();
-    String getUserName();
-    String getUserPassword();
-}
-
--- AEMAuthorPortfolioWebsiteCode/SimpleInt.java
+++
@@ -1,5 +0,0 @@
-package com.myTraining.core.services;
-
-public interface SimpleInt {
-    String getHelloWorld();
-}
